f3d5ac6fb9a526f57eba954f45be60295967e14c
Don't wake me up! <3300874438@qq.com>
2024-10-03 19:50:13 +0800

    Update 20241003回文数.cpp

--- leetcode代码/20241003回文数.cpp
+++ leetcode代码/20241003回文数.cpp
@@ -2,7 +2,7 @@ bool isPalindrome(int x) {
     if(x<0)return false;
     if(x==0)return true;
     long long  m=x,value=0;
-    while(m!=0){
+    while(m!=0){//实现x倒序
         value=value*10;
         value+=(m%10);
         m/=10;
